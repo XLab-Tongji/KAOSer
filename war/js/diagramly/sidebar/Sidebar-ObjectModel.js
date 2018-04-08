4f4fb0bc3dae1352129e010a6187f6e91cf9c5c8
LiDong <1225893478@qq.com>
2018-04-08 18:43:36 +0800

    KAOSer

---
+++ war/js/diagramly/sidebar/Sidebar-ObjectModel.js
@@ -0,0 +1,16 @@
+(function()
+{
+	// Adds ArchiMate 2.1 shapes
+	Sidebar.prototype.addArchiMatePalette = function()
+	{
+		this.addPaletteFunctions('archimate','Object Modeling', false,
+		[
+			this.createVertexTemplateEntry('shape=requirement;strokeWidth=3;whiteSpace=wrap;html=1;right=0;top=0;bottom=0;fillColor=#F5F5F5;routingCenterX=-0.5;strokeColor=#000000', 120, 40, '', 'Requirement'),
+			this.createVertexTemplateEntry('shape=hexagon;whiteSpace=wrap;html=1;top=0;bottom=0;fillColor=#FFFFFF;strokeColor=#000000', 80, 40, '', 'Agent'),
+			this.createVertexTemplateEntry('shape=event;whiteSpace=wrap;html=1;top=0;bottom=0;fillColor=#FFFFFF;strokeColor=#000000', 120, 40, '', 'Event'),
+			this.createVertexTemplateEntry('shape=ellipse;whiteSpace=wrap;html=1;top=0;bottom=0;fillColor=#ffff33;strokeColor=#000000', 25, 25, '', 'Operation'),
+			this.createVertexTemplateEntry('whiteSpace=wrap;html=1;top=0;bottom=0;fillColor=#FFFFFF;strokeColor=#000000', 120, 40, '', 'Entity'),
+		]);
+	};
+	
+})();
